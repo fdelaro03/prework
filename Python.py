fe9679430eb2713c32c13f6ed64d0a41d973d617
fdelaro03 <delarosaalexander227@gmail.com>
2021-08-03 15:53:59 -0400

    first commit

---
+++ Python.py
@@ -0,0 +1,67 @@
+#What are the data types? 
+#python data types: numeric: interger, float, and complex numer, dictionary, boolean, set sequence type: strings, list, and tuple. 
+
+#Strings
+from typing import Dict
+
+
+my_string = "hello world"
+print(my_string)
+my_string_with_apo_and_quote="steve's favorite movie is \"The Wizard of Oz\""
+print (my_string_with_apo_and_quote)
+
+#Integer
+
+
+
+#Float
+my_float=12.5
+
+#Adding ints
+my_int =5
+my_string = "5"
+#casting convert one dataytpe to another
+print(
+    str(my_int) + my_string)
+
+#Division
+print(
+    4/2)
+
+#Floor Division // or integer Division
+print(
+    5.75//2)
+print (4.5/6)
+
+#Modulo %
+print(4%4)
+
+#Boolean
+my_bool = True
+my_bool1= False
+#Boolean Expression evaluates to true or false
+#<, >, <=, >=, ==
+my_varl=100
+my_var2= 50
+my_var3=150
+my_var4=50
+
+print(my_var4 < my_var3)
+
+my_list = ["a", 55, 4.5, [9, 8, 7]]
+print(my_list[2], type(my_list[2]))
+
+#Dictionary
+my_dict = {
+    "name":"Kevin",
+    "age": 22,
+    "foods":["dimsum","sushi", "pizza", "tacos"]
+    "ice cream":{"chocolate":2.99, "vanilla": .99, "strawberry": 5.99}
+}
+
+Print (
+    my_dict ["foods" [1]],type(my_dict['food'][1])
+)
+
+my_list=[1, 2, 3, 4, 5, 6, , 7, 8, 9]
+print(my_)
